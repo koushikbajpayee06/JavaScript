a8a4004b7c44b78fce99608f6e452d677c8f39e0
Koushik <koushikbajpayee06@gmail.com>
2025-09-15 23:42:04 +0530

    promise.all succcess cases

---
+++ index.js
@@ -0,0 +1,16 @@
+const p1 = new Promise((resolve, reject)=>{
+    setTimeout(()=>resolve("p1 success"),3000)
+});
+
+const p2 = new Promise((resolve, reject)=>{
+    // setTimeout(()=>resolve("p2 success"),1000)
+    setTimeout(()=>resolve("p2 success"),1000)
+});
+
+const p3 = new Promise((resolve, reject)=>{
+    setTimeout(()=>resolve("p3 success"),2000)
+});
+
+Promise.all([p1,p2,p3]).then(res=>{
+    console.log(res);
+});
