f160d5184ff8a72e3af3eb10b55ef07795850bc9
Koushik <koushikbajpayee06@gmail.com>
2025-09-16 03:45:13 +0530

    handle promise with async & await

--- async-await/index2.js
+++ async-await/index2.js
@@ -3,8 +3,18 @@ const p = new Promise((resolve, reject)=>{
     resolve("Promise Successfully Resolved");
 });
 
-function getData(){
-    p.then((res)=>console.log(res));
+// function getData(){
+//     p.then((res)=>console.log(res));
+// }
+
+// getData();
+// await can only be use inside async function
+
+async function handlePromise(){
+     const val = await p;
+     console.log(val);
 }
 
-getData();
+ handlePromise();
+
+
