563c57922a1c72164ed04d798253c997c1bf6f4c
Koushik <koushikbajpayee06@gmail.com>
2025-09-15 23:55:30 +0530

    promise.all settled

---
+++ index1.js
@@ -0,0 +1,22 @@
+
+const p1 = new Promise((resolve, reject)=>{
+    setTimeout(()=>resolve("p1 success"),3000)
+});
+
+const p2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>resolve("p2 success"),1000)
+    // setTimeout(()=>reject("p2 failed"),1000)
+});
+
+const p3 = new Promise((resolve, reject)=>{
+    // setTimeout(()=>resolve("p3 success"),2000)
+    setTimeout(()=>reject("p3 failed"),2000)
+});
+
+Promise.allSettled([p1,p2,p3])
+.then(res=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.error(err);
+});
