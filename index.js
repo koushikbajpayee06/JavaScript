132f73090e5c458930ba5990bae60bda6eb61f9e
Koushik <koushikbajpayee06@gmail.com>
2025-09-15 23:52:02 +0530

    p-2 failed after 3 seconds

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const p1 = new Promise((resolve, reject)=>{
 });
 
 const p2 = new Promise((resolve, reject)=>{
-    // setTimeout(()=>resolve("p2 success"),1000)
-    setTimeout(()=>reject("p2 failed"),1000)
+    setTimeout(()=>resolve("p2 success"),1000)
+    // setTimeout(()=>reject("p2 failed"),1000)
 });
 
 const p3 = new Promise((resolve, reject)=>{
-    setTimeout(()=>resolve("p3 success"),2000)
+    // setTimeout(()=>resolve("p3 success"),2000)
+    setTimeout(()=>reject("p3 failed"),2000)
 });
 
-Promise.all([p1,p2,p3]).then(res=>{
+Promise.all([p1,p2,p3])
+.then(res=>{
     console.log(res);
 })
 .catch((err)=>{
