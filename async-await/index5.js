c740194c8c4f93e6caed951751e84d642c3bf374
Koushik <koushikbajpayee06@gmail.com>
2025-09-16 23:32:31 +0530

    resolved promise twice using async & await to check how the output behaves

--- async-await/index5.js
+++ async-await/index5.js
@@ -7,7 +7,12 @@ const p = new Promise((resolve, reject)=>{
 });
 
 async function getData(){
+    console.log("Hello world");
     const val = await p;
+    console.log(val);
+    console.log("Namaste JavaScript");
+    // resolved the value for two times, to see how the function behaves
+    const val2 = await p;
     console.log(val);
     console.log("Namaste JavaScript");
     
