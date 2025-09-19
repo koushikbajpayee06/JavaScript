631966eee46c2389e0aa7a8202bc4751e309cd85
Koushik <Koushikbajpayee06@gmail.com>
2025-09-19 16:24:30 +0530

    Make createOrder resolve orderId asynchronously with setTimeout

--- 03.Promise-Chaining&Error Handling/index.js
+++ 03.Promise-Chaining&Error Handling/index.js
@@ -2,24 +2,31 @@ const cart = ["shoe","pants","kurta"];
 
 const promise = createOrder(cart);
 
-promise.then(function(){
-    proceedToPayment(orderId)
+promise.then(function(orderId){
+    console.log(orderId)
 });
 
+/// Producer
 function createOrder(cart){
     const pr = new Promise(function(resolve, reject){
         // CreateOrder
         // ValidateCart
         // orderId
-        if(validateCard(cart)){
+        if(!validateCard(cart)){
             const err = new Error("cart is not Valid");
             reject(err);
         }
         //logic for crreateOrder
         const orderId = "12345";
         if(orderId){
-            resolve(orderId);
+            setTimeout(function(){
+                resolve(orderId)
+            },5000);
         }
     })
     return pr;
+}
+
+function validateCard(cart){
+    return true;
 }
