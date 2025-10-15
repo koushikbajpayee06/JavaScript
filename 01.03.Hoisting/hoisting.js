e4e3ac8026d07c81fba58861e9400e59edabbbab
Koushik <Koushikbajpayee06@gmail.com>
2025-10-16 01:25:19 +0530

    Hoisting for array function

--- 01.03.Hoisting/hoisting.js
+++ 01.03.Hoisting/hoisting.js
@@ -1,9 +1,10 @@
+
+
 getName();
 console.log(x);
-
+console.log(getName);
 var x = 7;
-
-function getName(){
-    console.log("Namaste Javascript")
+var getName = ()=>{
+    console.log("Namaste JavaScript");
 }
 
