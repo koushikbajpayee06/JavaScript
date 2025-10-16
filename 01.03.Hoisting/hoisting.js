7c4e879127fa461b0c1bc8fd96cd8b3f067e8916
Koushik <Koushikbajpayee06@gmail.com>
2025-10-16 23:31:53 +0530

    hoisting

--- 01.03.Hoisting/hoisting.js
+++ 01.03.Hoisting/hoisting.js
@@ -4,7 +4,16 @@ getName();
 console.log(x);
 console.log(getName);
 var x = 7;
+//Arrow function 
 var getName = ()=>{
     console.log("Namaste JavaScript");
+}
+//function expression Hoisted 
+var getName = function (){
+    console.log("Namaste JavaScript");
+}
+// function declearation
+function getName(){
+    console.log("namaste js");
 }
 
