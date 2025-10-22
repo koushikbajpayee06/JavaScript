063821d430fc9ecc2395137b1011af6ad1633ff2
Koushik <Koushikbajpayee06@gmail.com>
2025-10-22 16:39:53 +0530

    advance use of filter+reduce

---
+++ 01.19.Map-Filter-Reduce/filter-map.js
@@ -0,0 +1,12 @@
+const users =[
+    {firstName:"Koushik", lastName:"Bajpayee", age:26},
+    {firstName:"Aritra", lastName:"Bhaumik", age:45},
+    {firstName:"Abhishek", lastName:"Dhabal", age:67},
+    {firstName:"Jayeeta", lastName:"Barman", age:26},
+]
+
+const output = users.filter((x)=>x.age<30).reduce((acc,curr)=>{
+    acc.push(curr.firstName+" "+ curr.lastName);
+    return acc
+},[])
+console.log(output)
