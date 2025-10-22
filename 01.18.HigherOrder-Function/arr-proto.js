ed3e1c66fbc6fd1417eb4d81b51afe8d817ee16b
Koushik <Koushikbajpayee06@gmail.com>
2025-10-22 14:25:20 +0530

    added prototype

--- 01.18.HigherOrder-Function/arr-proto.js
+++ 01.18.HigherOrder-Function/arr-proto.js
@@ -12,14 +12,15 @@ const diameter = function(radius){
     return 2*radius;
 };
 
-const calculate = function (radius,logic){
+Array.prototype.calculate = function (logic){
     const output = [];
-    for(let i=0; i<radius.length; i++){
-        output.push(logic(radius[i]));
+    for(let i=0; i<this.length; i++){
+        output.push(logic(this[i]));
     }
     return output;
 };
 
-console.log(calculate(radius,area));
-console.log(calculate(radius, circumference));
-console.log(calculate(radius, diameter));
+console.log(radius.map(area));
+console.log(radius.calculate(area));
+// console.log(calculate(radius, circumference));
+// console.log(calculate(radius, diameter));
