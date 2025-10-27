21bb9bf9592c3093d7c869c5417af94a672e2184
Koushik <Koushikbajpayee06@gmail.com>
2025-10-27 18:17:26 +0530

    fetch function with async await

--- 02.04. async-await/index7.js
+++ 02.04. async-await/index7.js
@@ -4,6 +4,8 @@ const API_URL = "https://api.github.com/users/akshaymarch7";
 async function handlePromise(){
    const data =  await fetch(API_URL);
    const json = await data.json()
-   console.log(json)
+   console.log(json);
+
+//    fetch().then(res=>res.json()).then(res=>console.log(res))
 }
 handlePromise()
