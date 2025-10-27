6d710232813ee167dc7b225816fc269891500393
Koushik <Koushikbajpayee06@gmail.com>
2025-10-27 12:14:53 +0530

    promise chaining

--- 03.Promise-Chaining&Error Handling/index.js
+++ 03.Promise-Chaining&Error Handling/index.js
@@ -3,10 +3,18 @@ const cart = ["shoe","pants","kurta"];
 // create order is an async operation returns us a promise
 
 const promise = createOrder(cart);
+// console.log(promise)
 
 //attaching a cb to proceedToPayment that 
 promise.then(function(orderId){
-    console.log(orderId)
+    console.log(orderId);
+    return orderId;
+})
+.then(function(orderId){
+    return proceedToPayment(orderId);    
+})
+.then(function(paymentInfo){
+    console.log(paymentInfo);
 })
 .catch(function(err){
     console.log(err.message);
@@ -36,6 +44,11 @@ function createOrder(cart){
     return pr;
 }
 
+function proceedToPayment(orderId){
+    return new Promise(function( resolve, reject){
+        resolve("Payment Successfull");
+    });
+}
 function validateCard(cart){
     return true;
 }
